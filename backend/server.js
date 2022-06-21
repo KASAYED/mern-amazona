399cc99d275d0d9d897bb2e999bc0d393e19ea9e
KASAYED <kasayeddev.com>
2022-06-22 00:09:40 +0600

    video-08-fetch-data-from-backend-api

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,14 @@
-const data = require('./data.js')
+import express from 'express';
+import data from './data.js';
 
-const express = require('express')
+const app = express();
 
-const app = express()
-
-app.get('/api/products', (req, res)=>{
-    res.send(data.products)
-})
-
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-    console.log(`serve at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
