305703efa30016d0b73b3b2a752a6ae3f328140a
KASAYED <kasayeddev.com>
2022-06-23 16:06:04 +0600

    video-10-Add-ReactBootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Containter from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen'
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to="/">Amazona</Link>
+          <Navbar bg="dark" variant="dark">
+            <Containter>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazona</Navbar.Brand>
+              </LinkContainer>
+            </Containter>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen/>} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Containter>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Containter>
         </main>
+        <footer>
+          <div className='text-center'>@All Rights Reserve</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
