263833eeb462ba50c9fef9e2ce73524932ac3eb9
KASAYED <kasayeddev.com>
2022-06-26 22:29:44 +0600

    Video-21.-Seed-Sample-Users

--- backend/routes/seedRoute.js
+++ backend/routes/seedRoute.js
@@ -3,14 +3,17 @@
 // import data from '../data.js';
 const express = require('express');
 const Product = require('../models/productModel.js');
+const User = require('../models/userModel.js');
 const data = require('../data');
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
-  await Product.remove({});
+  await Product.remove({});s
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+  await User.remove({});
+  const createdUser = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUser });
 });
 // export default seedRouter;
 module.exports = seedRouter;
